c9d2be84766f48ab16b07e028d3b77563924ea5d
jacando.tos <sereicikas.t@gmail.com>
2020-04-27 17:04:57 +0200

    configure and add logo

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,16 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>Image Storage</div>
+  <div>Image<Logo class="logo" />Storage</div>
 </template>
 
 <script>
+import Logo from "../assets/file-archive-solid.svg";
+
 export default {
-  name: "AppHeader"
+  name: "AppHeader",
+  components: {
+    Logo
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logo {
+  height: 20px;
+  color: lightblue;
+}
+</style>
