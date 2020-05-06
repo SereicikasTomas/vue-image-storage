150daf97703938341285e28aa1aea83d5105ff4b
jacando.tos <sereicikas.t@gmail.com>
2020-05-06 15:51:31 +0200

    style image container

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <img v-for="image in allImages" :key="image.id" :src="image.link" />
+    <div v-if="isLoggedIn" class="image-container">
+      <img v-for="image in allImages" :key="image.id" :src="image.link" />
+    </div>
+    <h2 v-else>Log in to get started.</h2>
   </div>
 </template>
 
@@ -9,10 +12,30 @@ import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "ImageList",
-  computed: mapGetters(["allImages"]),
+  computed: mapGetters(["allImages", "isLoggedIn"]),
   methods: mapActions(["fetchImages"]),
   created() {
     this.fetchImages();
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.image-container {
+  column-count: 3;
+  column-gap: 2rem;
+
+  img {
+    width: 100%;
+    margin-bottom: 2rem;
+    border-radius: 5px;
+  }
+}
+
+h2 {
+  color: rgb(77, 170, 201);
+  font-size: 3rem;
+  font-weight: 300;
+  text-align: center;
+}
+</style>
